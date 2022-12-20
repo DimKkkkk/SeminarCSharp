87b55c0f2beae180b00e12af1808f3bb7ed9e461
DimK <kochergin.d.a@yandex.ru>
2022-12-20 18:39:28 +0300

    Добавил задачу 12 с функцией из семинара 2

---
+++ Sem2/Zadacha12Function/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 12 при помощи функции
+// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+int InsNum()
+{
+    int Usernum;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out Usernum))
+            break;
+        Console.WriteLine("Ты осёл, введи число!");
+    }
+    return Usernum;
+}
+
+int num1 = InsNum();
+int num2 = InsNum();
+
+int ost = num1 % num2;
+
+if (ost == 0)
+{
+    Console.WriteLine($"{num1}, {num2} -> кратно");
+}
+else
+{
+    Console.WriteLine($"{num1}, {num2} -> не кратно, остаток {ost}");
+}
