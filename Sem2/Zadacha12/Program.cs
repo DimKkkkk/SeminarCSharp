a76270f4856a148d2d46055edbf77a267a5380ed
DimK <kochergin.d.a@yandex.ru>
2022-12-20 16:45:17 +0300

    Добавил задачу 12 и 14 из семинара 2

---
+++ Sem2/Zadacha12/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+Console.Write("Введите первое число: ");
+string numA = Console.ReadLine() ?? "";
+Console.Write("Введите второе число: ");
+string numB = Console.ReadLine() ?? "";
+
+int num1 = int.Parse(numA);
+int num2 = int.Parse(numB);
+
+int ost = num1 % num2;
+
+if(ost == 0)
+{
+    Console.WriteLine($"{num1}, {num2} -> кратно");
+}
+else 
+{
+    Console.WriteLine($"{num1}, {num2} -> не кратно, остаток {ost}");
+}
---
+++ Sem2/Zadacha14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+Console.Write("Введите число: ");
+//string num1 = Console.ReadLine() ?? "";
+
+int number1 = int.Parse(Console.ReadLine() ?? "");
+if (number1 % 7 == 0)
+{
+    Console.WriteLine($"{number1} -> да");
+}
+else if (number1 % 23 == 0)
+{
+    Console.WriteLine($"{number1} -> да");
+}
+else
+    Console.WriteLine($"{number1} -> нет");
