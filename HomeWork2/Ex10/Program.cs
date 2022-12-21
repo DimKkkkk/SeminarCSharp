05a9e2b02ff248484f24dba438955e4192f661b1
DimK <kochergin.d.a@yandex.ru>
2022-12-21 13:38:32 +0300

    Добавил 3 задачи из домашки семинара 2

---
+++ HomeWork2/Ex10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("Введите трёхзначное число: ");
+int InsertNum = int.Parse(Console.ReadLine() ?? "");
+
+int num = InsertNum / 10;
+int num2 = num % 10;
+
+Console.WriteLine($"{InsertNum} -> {num2}");
---
+++ HomeWork2/Ex13/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите число: ");
+int InsertNum = int.Parse(Console.ReadLine() ?? "");
+
+int num3 = InsertNum % 10;
+int num = InsertNum;
+int num4 = InsertNum;
+
+if (InsertNum < 100)
+{
+    Console.WriteLine($"{InsertNum} -> третьей цифры тютю");
+}
+else if (InsertNum > 99 && InsertNum < 1000)
+{
+    Console.WriteLine($"{InsertNum} -> {num3}");
+}
+
+while (true)
+{
+    if (InsertNum > 999)
+    {
+        num = InsertNum / 100;
+        InsertNum = num;
+        num = InsertNum % 10;
+    }
+    break;
+}
+Console.WriteLine($"{num4} -> {num}");
