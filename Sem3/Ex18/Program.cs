dfac27d7b69175f2a1ec14b1b5e0039996db0094
DimK <kochergin.d.a@yandex.ru>
2022-12-22 22:11:21 +0300

    Добавлены задачи 18 и 21 из Семинара 3

---
+++ Sem3/Ex18/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y). */
+
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int Chetvert = int.Parse(Console.ReadLine() ?? "");
+
+if (Chetvert == 1)
+{
+    Console.WriteLine("X > 0 и Y > 0");
+}
+else if (Chetvert == 2)
+{
+    Console.WriteLine("X < 0 и Y > 0");
+}
+else if (Chetvert == 3)
+{
+    Console.WriteLine("X < 0 и Y < 0");
+}
+else if (Chetvert == 4)
+{
+    Console.WriteLine("X > 0 и Y < 0");
+}
+else
+    Console.WriteLine("Ошибка! Такой четверти не существует, введите от 1 до 4");
---
+++ Sem3/Ex21/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21 */
+
+Console.Clear();
+Console.Write("Введите координату точки X1: ");
+int X1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите координату точки Y1: ");
+int Y1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите координату точки X2: ");
+int X2 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите координату точки Y2: ");
+int Y2 = int.Parse(Console.ReadLine() ?? "");
+
+double result = Math.Sqrt(Math.Pow(X1-X2, 2)+Math.Pow(Y1-Y2, 2));
+Console.WriteLine($"X1 ({X1},{Y1}); Y1 ({X2},{Y2}) -> {Math.Round(result, 3)}"); //Math.Round округляет число результат.
