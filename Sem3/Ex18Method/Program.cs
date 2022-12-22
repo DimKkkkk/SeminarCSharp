1225f99b1558d4a20d76b51dd2d298f8533e2d4b
DimK <kochergin.d.a@yandex.ru>
2022-12-22 23:25:53 +0300

    Добавлена задач 18 с решением методом функции из Семинара 3

---
+++ Sem3/Ex18Method/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y). */
+
+static int InitQuaterByUser()
+{
+    try
+    {
+        Console.Write("Введите Номер координатной четверти: ");
+        int x=int.Parse(Console.ReadLine() ?? "");
+        return x;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
+
+static void PrintQuarterByCoords(int x)
+{
+    if(x==1)
+        Console.WriteLine("x>0 и y>0");
+    else if(x ==2)
+        Console.WriteLine("x<0 и y>0");
+    else if(x ==3)
+        Console.WriteLine("x<0 и y<0");
+    else if(x ==4)
+        Console.WriteLine("x>0 и y<0");
+    else
+        Console.WriteLine("Такой четверти не существует");
+}
+
+int x = InitQuaterByUser(); 
+PrintQuarterByCoords(x);
