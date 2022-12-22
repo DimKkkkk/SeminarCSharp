ac419634d6432d7991ce0ed6769400f4a6f83273
DimK <kochergin.d.a@yandex.ru>
2022-12-22 23:29:59 +0300

    Добавлена задач 21 с решением методом функции из Семинара 3

---
+++ Sem3/Ex21_Method/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21 */
+
+static void Dots_Coord(ref int x, ref int y)
+{
+    try
+    {
+        Console.Write("Введите координату X : ");
+        x = int.Parse(Console.ReadLine() ?? "");
+        Console.Write("Введите координату Y : ");
+        y = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    }
+}
+
+int x1=0, y1=0, x2=0, y2=0;
+
+Dots_Coord(ref x1, ref y1);
+Dots_Coord(ref x2, ref y2);
+
+double result = Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2));
+Console.WriteLine($"X1 ({x1},{y1}); Y1 ({x2},{y2}) -> {Math.Round(result, 3)}"); //Math.Round округляет число результат.
