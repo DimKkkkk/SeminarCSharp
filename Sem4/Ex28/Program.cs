63ec3b7277e38ef8cb1bb00c58fd68ecb8324a3c
DimK <kochergin.d.a@yandex.ru>
2023-01-13 15:57:41 +0300

    решил  задчау 28

--- Sem4/Ex28/Program.cs
+++ Sem4/Ex28/Program.cs
@@ -2,3 +2,39 @@
 4 -> 24 
 5 -> 120
 */
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число N: ", "Ошибка ввода!"); 
+int proizvedNumbers = GetProizNumbers(num); 
+Console.WriteLine($"{num} -> {proizvedNumbers}");
+
+int GetNumberFromUser(string message, string errorMessage) 
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber); 
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetProizNumbers(int number)
+/*{
+    int fac = 1; 
+    while(number > 0) 
+    {
+        fac *= number;  
+        number--;
+    }
+    return fac; 
+}*/
+{
+    int fac = 1; 
+    for(int i = 1; i <= number; i++) 
+    {
+        fac = fac * i;          
+    }
+    return fac; 
+}
