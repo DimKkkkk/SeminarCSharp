3b25407ce4bb0a7ac7ab3d9d584062aa026fa019
DimK <kochergin.d.a@yandex.ru>
2023-01-13 15:23:50 +0300

    решил задачу 26 и добавил 28

---
+++ Sem4/Ex26/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!"); 
+int count = GetCountNumber(num);
+Console.WriteLine($"{num} -> {count}");
+
+int GetNumberFromUser(string message, string errorMessage) 
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetCountNumber(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        number = number / 10;  // убираем последнее число введённого числа
+        result++; // увеличиваем result тем самым считается количество цифр в введённом числе
+    }
+    return result;
+}
