4a53d4b29be2febacbefec17d347f2c23881fb04
DimK <kochergin.d.a@yandex.ru>
2023-01-15 14:36:00 +0300

    Сделал задачу 27

---
+++ HomeWork4/Ex27/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!"); 
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+int GetNumberFromUser(string message, string errorMessage) 
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    int temp=0; 
+    while(number > 0) 
+    {
+        temp= number %10;
+        number = number / 10;
+        sum += temp;          
+    }
+    return sum; 
+}
