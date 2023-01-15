4b2fb2a1f088ad8feedda8b59c775b0c5d0755cf
DimK <kochergin.d.a@yandex.ru>
2023-01-15 14:54:34 +0300

    Сделал задачу 29

---
+++ HomeWork4/Ex29/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+PrintArr(); 
+
+void PrintArr ()
+
+{
+    Console.Write("["); 
+    for (int i = 0; i < 8; i++) 
+    {
+        int num = new Random().Next(0, 1000);
+        Console.Write($"{num}");
+        if (i < 7) Console.Write(", ");
+    }
+    Console.Write("]");
+}
