1023072ff26082c8064abbb1c890ac20c87c0e5b
DimK <kochergin.d.a@yandex.ru>
2023-01-18 14:09:37 +0300

    Добавил пробу решения задачи 29_2 и папку Sem5

---
+++ HomeWork4/Ex29_1/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    //Этот метод должен возвращать случайное значение
+    static int GetRandom()
+    {
+        //Создание объекта для генерации чисел (с указанием начального значения)
+        Random rnd = new Random();
+ 
+        //Получить случайное число 
+        int value = rnd.Next(0, 100);
+ 
+        //Вернуть полученное значение
+        return value;
+    }
+ 
+    static void Main(string[] args)
+    {        
+        //Вывод сгенерированных чисел в консоль
+        Console.Write("[");
+        Console.Write(GetRandom());
+        Console.Write(GetRandom());
+        Console.Write(GetRandom());
+        Console.Write("]");
+    }
+}
---
+++ HomeWork4/Ex29_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int N = CheckingTheArrayLengthEnteredByTheUser();
+double[] array = ArrayFormation(N);
+PrintArray(array)
+
+int CheckingTheArrayLengthEnteredByTheUser()
+
+double CheckingTheArrayLengthEnteredByTheUser()
+
+double[] ArrayFormation(int N)
+{
+    double[] userArray = new double[N];
+    int i = 0;
+    while (N > 0)
+    {
+        userArray[i] = CheckingTheArrayLengthEnteredByTheUser();
+        i++;
+        N--;
+    }
+    return userArray;
+}
