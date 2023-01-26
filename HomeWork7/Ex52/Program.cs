f2200bdf1afcda77b586f6511467c8ef3dea69ff
DimK <kochergin.d.a@yandex.ru>
2023-01-26 18:59:50 +0300

    решил задачу 52 среднее арифмет каждого столбца двумерного массива

---
+++ HomeWork7/Ex52/Program.cs
@@ -0,0 +1,75 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+Console.Clear();
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = GetNumberFromUser();
+
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = GetNumberFromUser();
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+Console.Write($"Среднее арифметическое каждого столбца: ");
+MidAr(array);
+
+
+int GetNumberFromUser()
+{
+    while(true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода");
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MidAr(int[,] inArray)
+{
+    int[] sum = new int[columns]; 
+    double mid = 0;
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum[j] += inArray[i, j];
+        }
+        mid = Math.Round((double)sum[j] / rows, 1);
+        if (j == inArray.GetLength(1) - 1) Console.Write($"{mid}.");
+        else Console.Write($"{mid}; ");
+    }    
+}
