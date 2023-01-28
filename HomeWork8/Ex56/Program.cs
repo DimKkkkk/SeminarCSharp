a372afff2fdc082895a40145b332c13078b82ca6
DimK <kochergin.d.a@yandex.ru>
2023-01-28 21:09:34 +0300

    сделал задачу 56 сумма каждой строки, какая строка с меньшей суммой

---
+++ HomeWork8/Ex56/Program.cs
@@ -0,0 +1,82 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+Console.Clear();
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = GetNumberFromUser();
+
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = GetNumberFromUser();
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+int index = SumRows(array);
+Console.WriteLine($"Строка с наименьшей суммой элементов: {index} строка");
+
+int GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода");
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumRows(int[,] inArray)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;     
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            sum += inArray[i, j];
+        }        
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        } 
+    }
+    return index;    
+}
