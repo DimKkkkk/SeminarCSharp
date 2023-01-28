730da4e87e14d15ab30b4022046bbb37b157e172
DimK <kochergin.d.a@yandex.ru>
2023-01-28 19:17:46 +0300

    сделал задачу 54 сортировка строки в массиве по убыванию

---
+++ HomeWork8/Ex54/Program.cs
@@ -0,0 +1,80 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая 
+упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+Console.Clear();
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = GetNumberFromUser();
+
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = GetNumberFromUser();
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
+
+int GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода");
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(1) - 1; k++)
+            {
+                if (inArray[i, k] < inArray[i, k + 1])
+                {
+                    int temp = inArray[i, k + 1];
+                    inArray[i, k + 1] = inArray[i, k];
+                    inArray[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
