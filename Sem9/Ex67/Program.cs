d33c504a7bd926ebd1a6c47ebc47502f2b815782
DimK <kochergin.d.a@yandex.ru>
2023-01-30 21:18:14 +0300

    сделали задачу 67 сумма чисел введённого числа при помощи рекурсии

---
+++ Sem9/Ex67/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+Console.Clear();
+
+int N = GetNumberFromUser("Введите значение N: ", "Ошибка ввода!");
+Console.WriteLine();
+Console.Write($"{N} -> ");
+Console.WriteLine(SumResult(N));
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+        if (isCorrect) return number;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+int SumResult(int number)
+{
+    if (number == 0) return 0;
+    else return number % 10 + SumResult(number / 10);
+}
