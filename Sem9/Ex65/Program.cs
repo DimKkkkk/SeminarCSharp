14cdded364e19ea893573de12ed84e39de70a5e8
DimK <kochergin.d.a@yandex.ru>
2023-01-30 22:32:32 +0300

    сделали задачу 65 вывод чисел в диапазоне при помощи рекурсии

--- Sem9/Ex65/Program.cs
+++ Sem9/Ex65/Program.cs
@@ -3,4 +3,30 @@ M = 1; N = 5 -> "1, 2, 3, 4, 5"
 M = 4; N = 8 -> "4, 6, 7, 8"
 */
 
+Console.Clear();
 
+Console.Write("Введите значение М: ");
+int numM = GetNumberFromUser();
+Console.Write("Введите значение N: ");
+int numN = GetNumberFromUser();
+
+
+Console.Write($"M = {numM}; N = {numN} -> ");
+Console.WriteLine(PrintResult(numM, numN));
+
+
+int GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+        if (isCorrect) return number;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+string PrintResult(int start, int end)
+{
+    if (start == end) return end.ToString(); 
+    else return start + ", " + PrintResult(start + 1, end);
+}
