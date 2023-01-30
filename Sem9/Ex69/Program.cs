1cde04bc55811f2e87d4fbe8140a5826f0b33f57
DimK <kochergin.d.a@yandex.ru>
2023-01-30 23:17:34 +0300

    сделали задачу 69 возводим число в степень при помощи рекурсии

---
+++ Sem9/Ex69/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+
+Console.Clear();
+
+Console.Write("Введите число А: ");
+int numA = GetNumberFromUser();
+Console.Write("Введите число В: ");
+int numB = GetNumberFromUser();
+
+Console.Write($"A = {numA}; B = {numB} -> {numA} в степени {numB} =  ");
+Console.WriteLine(StepenResult(numA, numB));
+
+
+int GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+        if (isCorrect) return number;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+int StepenResult (int number, int stepen)
+{
+    int result = 1;
+    if(stepen != 0) 
+        result = number * StepenResult(number, stepen - 1);
+    return result;
+}
