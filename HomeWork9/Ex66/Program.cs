f8d5b476e19e016e4de1aa6c0e7757111f6191b1
DimK <kochergin.d.a@yandex.ru>
2023-02-01 19:36:56 +0300

    Задача 66 рекурсией получить сумму элементов в заданном диапазоне

---
+++ HomeWork9/Ex66/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+
+Console.Write("Введите число М: ");
+int numM = GetNumberFromUser();
+Console.Write("Введите число N: ");
+int numN = GetNumberFromUser();
+
+Console.WriteLine();
+Console.Write($"M = {numM}; N = {numN} -> ");
+Console.WriteLine(SumResult(numM, numN));
+
+int GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+        if (isCorrect) return number;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+int SumResult(int start, int end)
+{
+    if (start == end) return start;
+    else if (start > end) return start + SumResult(start - 1, end + 1) + end;
+    else return start + SumResult(start + 1, end - 1) + end;
+}
